d0d1e0249d1a41db9155f7fb6545b15f27bf86fc
Brian Alonso <brian.alonso@gmail.com>
2013-02-26 19:49:38 -0800

    Initial Commit

---
+++ HW5 - Battleship/BDAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BDAAppDelegate.h
+//  HW5 - Battleship
+//
+//  Created by Brian Alonso on 2/26/13.
+//  Copyright (c) 2013 Brian Alonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HW5 - Battleship/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HW5 - Battleship
+//
+//  Created by Brian Alonso on 2/26/13.
+//  Copyright (c) 2013 Brian Alonso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BDAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BDAAppDelegate class]));
+    }
+}
